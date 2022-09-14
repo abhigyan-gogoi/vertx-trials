eea82715c881dd7ddf4f5446bef19e1475832402
abhig <abhigyan308@gmail.com>
2022-09-14 17:29:13 +0530

    Updated route to methods: Server.java

--- src/main/java/org/pupu/vertx_trials/server/Server.java
+++ src/main/java/org/pupu/vertx_trials/server/Server.java
@@ -6,6 +6,7 @@ import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class Server extends AbstractVerticle {
 
@@ -63,51 +64,22 @@ public class Server extends AbstractVerticle {
       // Now end the response
       context.response( ).end();
     });
+
     // Routing by exact path
-    router.route("/routing/exact-path/").handler(context -> {
-      // JSON response
-      context.json(
-        new JsonObject()
-          .put("Page", "Routing by Exact Path")
-      );
-    });
+    router.route("/routing/exact-path/").handler(this::routingExactPath);
+
     // Routing by path that begins with something
-    router.route("/routing/begin-something/*").handler(context -> {
-      // JSON response
-      context.json(
-        new JsonObject()
-          .put("Page", "Routing by Exact Path")
-          .put("Paths", "Starts with 'routing/begin-something/*")
-      );
-    });
+    router.route("/routing/begin-something/*").handler(this::routingBeginSomething);
+
     // Routing by capturing path parameters 1
-    router.route("/routing/:param1/:param2/").handler(context -> {
-      String param1 = context.pathParam("param1");
-      String param2 = context.pathParam("param2");
-      // JSON response
-      context.json(
-        new JsonObject()
-          .put("Page", "Routing by capturing path parameters 1")
-          .put("param1", param1)
-          .put("param2", param2)
-      );
-    });
+    router.route("/routing/:param1-:param2/").handler(this::routingParam1);
+
     // Routing by capturing path parameters 2
-    router.route("/routing/:param1-:param2/").handler(context -> {
-      String param1 = context.pathParam("param1");
-      String param2 = context.pathParam("param2");
-      // JSON response
-      context.json(
-        new JsonObject()
-          .put("Page", "Routing by capturing path parameters 2")
-          .put("param1", param1)
-          .put("param2", param2)
-      );
-    });
+    router.route("/routing/:param1-:param2/").handler(this::routingParam2);
+
     // Redirect to a new URL
-    router.route("/redirect/").handler(context -> {
-      context.redirect("https://vertx.io/docs/vertx-web/java/");
-    });
+    router.route("/redirect/").handler(this::redirectURL);
+
 
     // Start server on port 8888
     server
@@ -121,5 +93,55 @@ public class Server extends AbstractVerticle {
         }
       );
 
+  }
+
+  // Method for Routing by exact path
+  private void routingExactPath(RoutingContext routingContext) {
+    // JSON response
+    routingContext.json(
+      new JsonObject()
+        .put("Page", "Routing by Exact Path")
+    );
+  }
+
+  // Method for Routing by path that begins with something
+  private void routingBeginSomething(RoutingContext routingContext) {
+    // JSON response
+    routingContext.json(
+      new JsonObject()
+        .put("Page", "Routing by Exact Path")
+        .put("Paths", "Starts with 'routing/begin-something/*")
+      );
+  }
+
+  // Method for Routing by capturing path parameters 1
+  private void routingParam1(RoutingContext routingContext) {
+    String param1 = routingContext.pathParam("param1");
+    String param2 = routingContext.pathParam("param2");
+    // JSON response
+    routingContext.json(
+      new JsonObject()
+        .put("Page", "Routing by capturing path parameters 1")
+        .put("param1", param1)
+        .put("param2", param2)
+    );
+  }
+
+  // Method for Routing by capturing path parameters 2
+  private void routingParam2(RoutingContext routingContext) {
+    String param1 = routingContext.pathParam("param1");
+    String param2 = routingContext.pathParam("param2");
+    // JSON response
+    routingContext.json(
+      new JsonObject()
+        .put("Page", "Routing by capturing path parameters 2")
+        .put("param1", param1)
+        .put("param2", param2)
+    );
+  }
+
+  // Method to redirect to a new URL
+  private void redirectURL(RoutingContext routingContext) {
+    routingContext.redirect("https://vertx.io/docs/vertx-web/java/");
   }
 }
