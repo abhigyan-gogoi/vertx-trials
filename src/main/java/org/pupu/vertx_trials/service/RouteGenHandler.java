4f6ce1b42a82912689e75c08bc969f5a65475fbb
abhigyan-gogoi <abhigyank.gogoi@zirohcom>
2022-09-28 18:39:39 +0530

    Did not add file to previous commit;

--- src/main/java/org/pupu/vertx_trials/service/RouteGenHandler.java
+++ src/main/java/org/pupu/vertx_trials/service/RouteGenHandler.java
@@ -1,13 +1,12 @@
 package org.pupu.vertx_trials.service;
 
 import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import org.pupu.vertx_trials.model.Database;
 
 public interface RouteGenHandler extends Handler<RoutingContext> {
-  static RouteGenHandler create(Database db, Vertx vertx, Router router){
-    return new RouteGenHandlerImpl(db, vertx, router);
+  static RouteGenHandler create(Database db, Router router){
+    return new RouteGenHandlerImpl(db, router);
   }
 }
